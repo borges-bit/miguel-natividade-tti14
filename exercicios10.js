fb66926d29494a3b3b90377d65e10e9b8761773d
borges-bit <andreborgespereira2009@gmail.com>
2026-06-16 15:52:52 -0300

    Create exercicios10.js

---
+++ exercicios10.js
@@ -0,0 +1,13 @@
+let litrosComsumidos
+let precoLitro
+let custoTotal
+
+litrosconsumidos=prompt("Digite a quantidade de litros consumidos:")
+precoLitro=prompt("Digite o preco do litro de combustivel: R$")
+
+litrosConsumidos=parseFloat(litrosConsumidos)
+precoLitro=parseFloat(precoLitro)
+
+custoTotal=litrosConsumidos * precoLitro
+
+alert("Custo total de abastecimento = R$ " + custo total
