423b4ec33968805696226413878f88417637db36
borges-bit <andreborgespereira2009@gmail.com>
2026-06-16 14:07:28 -0300

    Add student grade calculation script

---
+++ exercicio4.js
@@ -0,0 +1,16 @@
+let nota1
+let nota2
+let nota3
+let mdeiAluno
+
+nota1 = prompt("informe a nota 1:")
+nota2 = prompt(informe a nota 2:")
+nota3 = prompt(informe a nota 3:")
+
+nota1 = parseFloat(nota1)
+nota2 = parseFloat(nota2)
+nota3 = parseFloat(nota3)
+
+mediaAluno=(nota1 + nota2 + nota3) / 3
+
+alert("Media do aluno = " + mediaAluno)
