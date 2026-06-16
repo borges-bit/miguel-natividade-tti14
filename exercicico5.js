de7ac730794ea706c4542c60bdd835b47e1c8db9
borges-bit <andreborgespereira2009@gmail.com>
2026-06-16 14:15:45 -0300

    Create exercicico5.js

---
+++ exercicico5.js
@@ -0,0 +1,10 @@
+let areaParede
+let litrosTinta
+
+areasParede = prompt("informe a area da parede em m2:")
+
+areaParede = parseFloat(areaParede)
+
+litrostinta = areaParede / 5
+
+alert("Quantidade de tinta necessaria = " + litostinta + " litos")
