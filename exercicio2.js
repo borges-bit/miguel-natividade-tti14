d8aaf4f2d559b2016c815cca471399e9dfe5ac38
borges-bit <andreborgespereira2009@gmail.com>
2026-06-16 13:46:22 -0300

    Add weekly salary calculation in exercicio2.js

---
+++ exercicio2.js
@@ -0,0 +1,13 @@
+let horasTrabalhadas
+let valorHora
+let salarioSemanal1
+
+horasTrabalhadas = prompt("INFORME A QUANTIDADE HORAS TRABALHADAS:")
+valorHora = prompt("INFORME A QUANTIDADE A SER PAGA: R$")
+
+horasTrabalhadas = parseFloat(horasTrabalhadas)
+valorhora = parseFloat(valorHora)
+
+salarioSemanal = horastrabalhadas * valorHora
+
+alert("salario semana1 = + salarioSemanal)
