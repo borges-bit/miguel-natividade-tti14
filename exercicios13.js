9815fffe864bf7f4f2bfd627c708a3c4ff183eaf
borges-bit <andreborgespereira2009@gmail.com>
2026-06-16 16:24:32 -0300

    Create exercicios13.js

---
+++ exercicios13.js
@@ -0,0 +1,13 @@
+let distanciaKm
+let temoHoras
+let velocidadeMedia
+
+distanciaKm = prompt("Digite a distancia pecorrida (km):")
+tempoHoras = prompt("Digite o tempo gasto no trajeto (horas):")
+
+distancia = parseFloat(distanciaKm)
+tempoHoras = parseFloat(tempoHoras)
+
+velocidadeMedia = " + velocidadeMedia + " km/h")
+
+alert("Velocidade media = " + velocidadeMedia + " km/h")
