e1cc8e63b06748af7297e5f601a41df9494856c9
borges-bit <andreborgespereira2009@gmail.com>
2026-06-16 14:47:25 -0300

    Create exercicio9.js

---
+++ exercicio9.js
@@ -0,0 +1,13 @@
+let peso
+let altura
+let imc
+
+peso=prompt("Digite o peso (kg):")
+altura=prompt("Digite a altura (m):")
+
+peso=parseFloat(peso)
+altura=parseFloat(altura)
+
+imc=peso/altura * altura)
+
+alert("imc = "+imc)
