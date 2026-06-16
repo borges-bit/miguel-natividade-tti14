53a531c20597b391f2dd77438bf35b2279cc799f
borges-bit <andreborgespereira2009@gmail.com>
2026-06-16 16:13:37 -0300

    Create exercicio12.js

---
+++ exercicio12.js
@@ -0,0 +1,10 @@
+let idadeAnos
+let mesesVida
+
+idadeAnos=prompt("Digite sua idade em anos:")
+
+idadeAnos=parseInt(idadeAnos)
+
+mesesVida=idadeAnos * 12
+
+alert("Voce possui " + mesesVida + " meses de vida")
